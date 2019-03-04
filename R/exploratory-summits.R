3fa4aad0c293cdfc41fd42fb54400b538ce0bc41
nlangholz <nathan.langholz@redbull.com>
2019-03-03 16:56:08 -0800

    add plot v1

--- R/exploratory-summits.R
+++ R/exploratory-summits.R
@@ -3,6 +3,9 @@ pacman::p_load(feather,leaflet, ggmap,magrittr,dplyr)
 
 members <- read_feather("data/members.feather")
 
+#this is dumb; but works
+members$MYEAR <- as.numeric(levels(members$MYEAR))[as.numeric(members$MYEAR)]
+
 #total number of climber ids; 37034
 length(unique(members$CLIMBERID))
 
@@ -37,6 +40,25 @@ yearly_summits <- members %>%
   group_by(PEAKID,MYEAR) %>%
   summarise(SUMMITS = sum(MSUCCESS)) %>%
   arrange(desc(MYEAR)) %>% 
+  ungroup()
 
-plot(yearly_summits %>% filter(PEAKID == 'EVER') %>% select(SUMMITS))
+plot( %>% filter(PEAKID == 'EVER') %>% select(SUMMITS))
+
+
+
+1 EVER      9044
+2 AMAD      4084
+3 CHOY      3709
+4 MANA      1460
+5 LHOT       777
+6 BARU       628
+7 DHA1       543
+8 PUMO       510
+9 MAKA       484
+10 HIML       464
+
+e1 <- which(yearly_summits$PEAKID == 'MANA')
+plot(seq(1900,2020,10),seq(100,1300,100),ylim = c(0,1000),type = 'n',ylab = 'Summits',xlab = 'Year')
+points(yearly_summits$MYEAR[e1],yearly_summits$SUMMITS[e1])
+points(yearly_summits$MYEAR[e1],yearly_summits$SUMMITS[e1],type = 'l')
 
