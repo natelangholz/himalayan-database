6d8306d1d2946a9e03edecb2624a63b164d0a3db
nlangholz <nathan.langholz@redbull.com>
2019-03-06 21:58:12 -0800

    add a new plot, new points, new colors

--- R/exploratory-mapping.R
+++ R/exploratory-mapping.R
@@ -9,7 +9,10 @@ peaks <- read_feather("data/peaks.feather")
 
 
 mybins=seq(5000, 9000, by=5000)
-mypalette = colorBin( palette="YlOrBr", domain=peaks$HEIGHTM, na.color="transparent", bins=mybins)
+mypalette = colorBin( palette="BuPu", domain=peaks$HEIGHTM, na.color="transparent", bins=mybins)
+#BuPu
+#YlOrBr
+#Purples
 
 mytext=paste( "Name: ", peaks$PKNAME, "<br/>", "Height: ", peaks$HEIGHTM, sep="") %>%
   lapply(htmltools::HTML)
@@ -26,7 +29,7 @@ leaflet(peaks) %>%
   ) 
 
 #https://leaflet-extras.github.io/leaflet-providers/preview/
-
+?addRasterImage()
 ###########################################################
 
 # Easy to make it interactive!
@@ -42,13 +45,13 @@ peaks %>%
   mutate(HEIGHTM=HEIGHTM/1000) %>%
   ggplot() +
   geom_polygon(data = NEPAL, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
-  geom_point(  aes(x=long, y=lat, size=pop, color=pop, alpha=pop), shape=20, stroke=FALSE) +
+  geom_point(data = peaks,  aes(x=lon, y=lat, size=HEIGHTM, color=HEIGHTM, alpha=HEIGHTM), shape=20, stroke=FALSE) +
   scale_size_continuous(name="Population (in M)", trans="log", range=c(1,12), breaks=mybreaks) +
   scale_alpha_continuous(name="Population (in M)", trans="log", range=c(0.1, .9), breaks=mybreaks) +
   scale_color_viridis(option="magma", trans="log", breaks=mybreaks, name="Population (in M)" ) +
-  theme_void() + ylim(50,59) + coord_map() + 
+  theme_void()  + coord_map() + 
   guides( colour = guide_legend()) +
-  ggtitle("The 1000 biggest cities in the UK") +
+  ggtitle("The peaks in Nepal") +
   theme(
     legend.position = c(0.85, 0.8),
     text = element_text(color = "#22211d"),
@@ -63,20 +66,19 @@ peaks %>%
 
 # plot
 p=peaks %>%
-  
+  filter(MSUMMIT8K == 1) %>%
   arrange(HEIGHTM) %>%
   #mutate( name=factor(name, unique(name))) %>%
   mutate( mytext=paste("PEAK: ", PEAKID, "\n", "HEIGHT M: ", HEIGHTM, sep="")) %>%  # This prepare the text displayed on hover.
   # Makte the static plot calling this text:
   ggplot() +
   geom_polygon(data = NEPAL, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
-  geom_point(data = peaks,aes(x=lon, y=lat, size=HEIGHTM, color=HEIGHTM, text=mytext, alpha=HEIGHTM) ) +
+  geom_point(aes(x=lon, y=lat, size=HEIGHTM/1000, color=HEIGHTM, text=mytext, alpha=HEIGHTM) , shape = 2 ) +
   
-  scale_size_continuous(range=c(1,15)) +
+  scale_size_continuous(range=c(1,4)) +
   scale_color_viridis(option="inferno", trans="log" ) +
   scale_alpha_continuous(trans="log") +
   theme_void() +
-  ylim(50,59) +
   coord_map() +
   theme(legend.position = "none")
 
